7b2bbd4f36910ddd29026b34e1c6eda0a0fe1719
chris thorn <delathornesto@yahoo.com>
2016-10-04 08:59:08 -0700

    seed is now username

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "christopher",
+User.create!(username: "christopher",
 			 email: "chri3topher_t@hotmail.com",
 			 password: "Spartan117",
 			 password_confirmation: "Spartan117",
