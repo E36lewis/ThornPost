ea40c07b7647520de8bb70a0330f53ce069d0273
chris thorn <delathornesto@yahoo.com>
2016-10-04 12:09:34 -0700

    trying current_story in stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,7 +5,7 @@ class StoriesController < ApplicationController
    layout "editor", only: [:new, :edit, :create, :update]
    
   def show
-    @story = Story.find(params[:id])
+    @story = current_story
     @responses = @story.responses.includes(:user)
     @related_stories = @story.related_stories
     # If an old id or a numeric id was used to find the record, then
