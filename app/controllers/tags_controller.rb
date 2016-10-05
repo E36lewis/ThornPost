089e755d4820edb45dd953f843fdce01b776404d
chris thorn <delathornesto@yahoo.com>
2016-10-05 10:27:27 -0700

    added friendly to set_tag @tag = Tag.friendly.find(params[:id])

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,7 +9,7 @@ class TagsController < ApplicationController
   private
 
     def set_tag
-      @tag = Tag.find(params[:id])
+      @tag = Tag.friendly.find(params[:id])
     end
 
     def tagged_stories
