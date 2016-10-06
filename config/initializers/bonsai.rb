74367b1b7d50603060659bca4e153067d96a8e65
chris thorn <delathornesto@yahoo.com>
2016-10-06 09:37:58 -0700

    forgot initlizer file

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,6 +1,6 @@
-#require 'elasticsearch/model'
+require 'elasticsearch/model'
 
-#if ENV['BONSAI_URL']
-#  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-#  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
-#end
+if ENV['BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
+end
